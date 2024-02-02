52270b4a0a4e7306d628d163ddfa6f0e8e6ec9a8
Julien FRANCAIS <jfrancai@student.42.fr>
2024-02-02 23:01:50 +0100

    wip

--- utils/notion.ts
+++ utils/notion.ts
@@ -1,5 +1,5 @@
 import "server-only";
-
+import { cache } from "react";
 import { NotionAPI } from 'notion-client'
 
 const notion = new NotionAPI({
@@ -8,49 +8,7 @@ const notion = new NotionAPI({
 });
 
 
-import { Client } from "@notionhq/client";
-import {
-  BlockObjectResponse,
-  PageObjectResponse,
-} from "@notionhq/client/build/src/api-endpoints";
-import { cache } from "react";
-
-export const notionClient = new Client({
-  auth: process.env.NOTION_TOKEN
-})
 
 export const getRecordMap = cache(() => {
   return notion.getPage('60c115eee4854ba9ba85bfd872a3cad5');
 })
-
-export const getPages = cache(() => {
-  return notionClient.databases.query({
-    filter: {
-      property: "Status",
-      select: {
-        equals: "Published",
-      },
-    },
-    database_id: process.env.NOTION_DATABASE_ID!,
-  });
-});
-
-export const getPageContent = cache(async (pageId: string) => {
-  return notionClient.blocks.children
-    .list({ block_id: pageId })
-    .then((res) => res.results as BlockObjectResponse[]);
-});
-
-export const getPageBySlug = cache(async (slug: string) => {
-  return notionClient.databases
-    .query({
-      database_id: process.env.NOTION_DATABASE_ID!,
-      filter: {
-        property: "Slug",
-        rich_text: {
-          equals: slug,
-        },
-      },
-    })
-    .then((res) => res.results[0] as PageObjectResponse | undefined);
-});
