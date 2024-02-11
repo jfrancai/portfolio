2be7a8aa2ab6fe4faedb003a1d802cd28547e6ec
Julien FRANCAIS <jfrancai@student.42.fr>
2024-02-11 13:47:59 +0100

    copied the rest of the files

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
 import dotenv from "dotenv";
+import fs from "fs-extra";
 import { loadConfig } from "./config";
+import {
+  Client,
+  isFullPageOrDatabase,
+  iteratePaginatedAPI,
+} from "@notionhq/client";
+import { savePage } from "./render";
 
 dotenv.config();
 
@@ -7,7 +14,29 @@ async function main() {
   if (process.env.NOTION_TOKEN === "")
     throw Error("The NOTION_TOKEN environment vairable is not set.");
   const config = await loadConfig();
-  console.info("[Info] Config loaded ", JSON.stringify(config));
+  console.info("[Info] Config loaded ");
+
+  const notion = new Client({
+    auth: process.env.NOTION_TOKEN,
+  });
+
+  const page_ids: string[] = [];
+  console.info("[Info] Start processing mounted databases");
+
+  // process mounted databases
+  for (const mount of config.mount.databases) {
+    fs.ensureDirSync(`content/projects${mount.target_folder}`);
+    for await (const page of iteratePaginatedAPI(notion.databases.query, {
+      database_id: mount.database_id,
+    })) {
+      if (!isFullPageOrDatabase(page) || page.object !== "page") {
+        continue;
+      }
+      console.info(`[Info] Start processing page ${page.id}`);
+      page_ids.push(page.id);
+      await savePage(page, notion, mount);
+    }
+  }
 }
 
 main()
