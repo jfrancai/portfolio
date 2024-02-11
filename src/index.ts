2a650b64aa35c4f28d027a0917a3a799711a842f
Julien FRANCAIS <jfrancai@student.42.fr>
2024-02-11 12:25:20 +0100

    config

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,18 @@
 import dotenv from "dotenv";
+import { loadConfig } from "./config";
 
 dotenv.config();
 
-console.log(dotenv);
+async function main() {
+  if (process.env.NOTION_TOKEN === "")
+    throw Error("The NOTION_TOKEN environment vairable is not set.");
+  const config = await loadConfig();
+  console.info("[Info] Config loaded ");
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((err: any) => {
+    console.log(err);
+    process.exit(1);
+  });
