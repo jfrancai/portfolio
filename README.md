8386b331b8f8960658895cb8aefc10ede3adeb2a
Julien FRANCAIS <jfrancai@student.42.fr>
2025-09-21 15:22:57 +0200

    first commit

--- README.md
+++ README.md
@@ -1,26 +1,11 @@
-[![Netlify Status](https://api.netlify.com/api/v1/badges/a3e898e1-f358-4ccc-8436-41da8cde35ca/deploy-status)](https://app.netlify.com/sites/jfrancai/deploys)
-
-## Local Development
-
-Pre-requisites: [Hugo](https://gohugo.io/getting-started/installing/), [Go](https://golang.org/doc/install) and [Git](https://git-scm.com)
-
-```shell
-# Clone the repo
-git clone https://github.com/jfrancai/portfolio.git
-
-# Change directory
-cd portfolio
-
-# Start the server
-hugo mod tidy
-hugo server --logLevel debug --disableFastRender -p 1313
+To install dependencies:
+```sh
+bun install
 ```
 
-### Update theme
-
-```shell
-hugo mod get -u
-hugo mod tidy
+To run:
+```sh
+bun run dev
 ```
 
-See [Update modules](https://gohugo.io/hugo-modules/use-modules/#update-modules) for more details.
+open http://localhost:3000
--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,9 @@
-import dotenv from "dotenv";
-import fs from "fs-extra";
-import { loadConfig } from "./config";
-import {
-  Client,
-  isFullPageOrDatabase,
-  iteratePaginatedAPI,
-} from "@notionhq/client";
-import { savePage } from "./render";
+import { Hono } from 'hono'
 
-dotenv.config();
+const app = new Hono()
 
-async function main() {
-  if (process.env.NOTION_TOKEN === "")
-    throw Error("The NOTION_TOKEN environment vairable is not set.");
-  const config = await loadConfig();
-  console.info("[Info] Config loaded ");
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
 
-  const notion = new Client({
-    auth: process.env.NOTION_TOKEN,
-  });
-
-  const page_ids: string[] = [];
-  console.info("[Info] Start processing mounted databases");
-
-  // process mounted databases
-  for (const mount of config.mount.databases) {
-    fs.ensureDirSync(`content/projects/${mount.target_folder}`);
-    for await (const page of iteratePaginatedAPI(notion.databases.query, {
-      database_id: mount.database_id,
-    })) {
-      if (!isFullPageOrDatabase(page) || page.object !== "page") {
-        continue;
-      }
-      console.info(`[Info] Start processing page ${page.id}`);
-      page_ids.push(page.id);
-      await savePage(page, notion, mount);
-    }
-  }
-}
-
-main()
-  .then(() => process.exit(0))
-  .catch((err: any) => {
-    console.log(err);
-    process.exit(1);
-  });
+export default app
